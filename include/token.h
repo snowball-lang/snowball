a4a6d3bc16f53e94d48ec5f9c97a77caf4300b95
mauro-balades <mauro.balades@tutanota.com>
2022-04-28 20:55:40 +0200

    created a Token structure

--- include/token.h
+++ include/token.h
@@ -1,5 +1,6 @@
 
 #include <string>
+#include <vector>
 
 #ifndef __SNOWBALL_TOKEN_H_
 #define __SNOWBALL_TOKEN_H_
@@ -32,10 +33,9 @@ namespace snowball {
         BUILTIN_TYPE,
 
         // Literal values
-        VALUE_INT,
+        VALUE_NUMBER,
         VALUE_NULL,
         VALUE_BOOL,
-        VALUE_FLOAT,
         VALUE_STRING,
 
         /*
@@ -181,6 +181,131 @@ namespace snowball {
         _EOF,              // End of file. This already exists in <stdio.h>
         UNKNOWN,           // Other
         _TK_MAX_,          // Max token
+    };
+
+    struct Token {
+        TokenType type = TokenType::UNKNOWN;
+        int line = 0, col = 0;
+
+        std::string value;
+
+        Token() {}
+        Token(TokenType p_type) { type = p_type; }
+
+        std::string to_string() const {
+            switch (type) {
+
+                // Symbols
+                case TokenType::SYM_AT:             return "@";
+                case TokenType::SYM_DOT:            return ".";
+                case TokenType::SYM_HASH:           return "#";
+                case TokenType::SYM_COMMA:          return ",";
+                case TokenType::SYM_COLLON:         return ":";
+                case TokenType::SYM_DOLLAR:         return "$";
+                case TokenType::SYM_QUESTION:       return "?";
+                case TokenType::SYM_SEMI_COLLON:    return ";";
+
+                // Brackets
+                case TokenType::BRACKET_LCURLY:     return "{";
+                case TokenType::BRACKET_RCURLY:     return "}";
+                case TokenType::BRACKET_LPARENT:    return "(";
+                case TokenType::BRACKET_RPARENT:    return ")";
+                case TokenType::BRACKET_LSQUARED:   return "[";
+                case TokenType::BRACKET_RSQUARED:   return "]";
+
+                // Equiality
+                case TokenType::OP_GT:              return ">";
+                case TokenType::OP_LT:              return "<";
+                case TokenType::OP_GTEQ:            return ">=";
+                case TokenType::OP_EQEQ:            return "==";
+                case TokenType::OP_LTEQ:            return "<=";
+                case TokenType::OP_NOTEQ:           return "!=";
+
+                // Mathematical symbols
+                case TokenType::OP_MOD:             return "%";
+                case TokenType::OP_DIV:             return "/";
+                case TokenType::OP_MUL:             return "*";
+                case TokenType::OP_PLUS:            return "+";
+                case TokenType::OP_MINUS:           return "-";
+                case TokenType::OP_MULEQ:           return "*=";
+                case TokenType::OP_DIVEQ:           return "/=";
+                case TokenType::OP_PLUSEQ:          return "+=";
+                case TokenType::OP_MOD_EQ:          return "%=";
+                case TokenType::OP_MINUSEQ:         return "-=";
+
+                // Asignment
+                case TokenType::OP_EQ:              return "=";
+                case TokenType::OP_OR:              return "||";
+                case TokenType::OP_AND:             return "&&";
+                case TokenType::OP_NOT:             return "!";
+
+                // Bitwise operations
+                case TokenType::OP_BIT_OR:          return "|";
+                case TokenType::OP_BIT_NOT:         return "~";
+                case TokenType::OP_BIT_AND:         return "&";
+                case TokenType::OP_BIT_XOR:         return "^";
+                case TokenType::OP_BIT_OR_EQ:       return "|=";
+                case TokenType::OP_BIT_LSHIFT:      return "<<";
+                case TokenType::OP_BIT_RSHIFT:      return ">>";
+                case TokenType::OP_BIT_AND_EQ:      return "&=";
+                case TokenType::OP_BIT_XOR_EQ:      return "^=";
+                case TokenType::OP_BIT_LSHIFT_EQ:   return "<<=";
+                case TokenType::OP_BIT_RSHIFT_EQ:   return ">>=";
+
+                // Identifiers
+                case TokenType::IDENTIFIER:         return value;
+
+                // Keywods
+                case TokenType::KWORD_IMPORT:       return "import";
+                case TokenType::KWORD_CLASS:        return "class";
+                case TokenType::KWORD_ENUM:         return "enum";
+                case TokenType::KWORD_FUNC:         return "func";
+                case TokenType::KWORD_VAR:          return "var";
+                case TokenType::KWORD_CONST:        return "const";
+                case TokenType::KWORD_NULL:         return "null";
+                case TokenType::KWORD_TRUE:         return "true";
+                case TokenType::KWORD_FALSE:        return "false";
+                case TokenType::KWORD_IF:           return "if";
+                case TokenType::KWORD_ELSE:         return "else";
+                case TokenType::KWORD_WHILE:        return "while";
+                case TokenType::KWORD_FOR:          return "for";
+                case TokenType::KWORD_SWITCH:       return "switch";
+                case TokenType::KWORD_CASE:         return "case";
+                case TokenType::KWORD_DEFAULT:      return "default";
+                case TokenType::KWORD_BREAK:        return "break";
+                case TokenType::KWORD_CONTINUE:     return "continue";
+                case TokenType::KWORD_STATIC:       return "static";
+                case TokenType::KWORD_THIS:         return "this";
+                case TokenType::KWORD_SUPER:        return "super";
+                case TokenType::KWORD_RETURN:       return "return";
+                case TokenType::KWORD_AND:          return "and";
+                case TokenType::KWORD_OR:           return "or";
+                case TokenType::KWORD_NOT:          return "not";
+
+                // Literal values
+                case TokenType::VALUE_NULL:         return "null";
+
+                case TokenType::VALUE_NUMBER:
+                case TokenType::VALUE_BOOL:
+                    return value;
+
+                case TokenType::VALUE_STRING:
+                    return std::string("\"") + value + "\"";
+
+                // Other
+                case TokenType::UNKNOWN: return "<unknown>";
+                case TokenType::_EOF:    return "<eof>";
+                case TokenType::_TK_MAX_: return "<_TK_MAX_>";
+
+                default:
+                    return "<BUG>";
+            }
+
+            // TODO: throw error: missed enum in switch case
+        }
+
+        std::vector<int, int> get_pos() const {  return std::vector<int, int>(line, col);  }
+        uint32_t get_width() const { return (uint32_t)to_string().size(); }
     };
 }
 
