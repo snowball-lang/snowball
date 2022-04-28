0fbbfedf2a663fadeceeb8bd8227448690abe306
mauro-balades <mauro.balades@tutanota.com>
2022-04-28 16:47:31 +0200

    added a source info class to let the programm know some info from it's source

--- include/snowball.h
+++ include/snowball.h
@@ -2,8 +2,10 @@
 #ifndef __SNOWBALL_H_
 #define __SNOWBALL_H_
 
+#include "lexer.h"
 #include "logger.h"
 #include "errors.h"
 #include "compiler.h"
+#include "source_info.h"
 
 #endif // __SNOWBALL_H_
