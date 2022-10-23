7c6761ebcb357296b5855002f4cbdd3e52d460a6
mauro-balades <mauro.balades@tutanota.com>
2022-10-23 12:55:22 +0200

    added prefix path for both debug and release scripts

--- build_scripts/dbg.sh
+++ build_scripts/dbg.sh
@@ -1,3 +1,3 @@
 mkdir -p bin/Debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DEXECUTABLE_OUTPUT_PATH="Debug" .
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Debug -DEXECUTABLE_OUTPUT_PATH="Debug" .
 cmake --build .
--- build_scripts/release.sh
+++ build_scripts/release.sh
@@ -1,3 +1,3 @@
 mkdir -p bin/Release
-cmake -DCMAKE_BUILD_TYPE=Release -DEXECUTABLE_OUTPUT_PATH="Release" .
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release -DEXECUTABLE_OUTPUT_PATH="Release" .
 cmake --build .
