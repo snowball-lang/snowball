aea6553b692030a48549b17b634bab9e305277d5
mauro-balades <mauro.balades@tutanota.com>
2023-03-25 21:20:04 +0100

    Updating path when installing

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,28 +1,90 @@
 #!/usr/bin/env bash
 set -e
 
-# function to update the shell configuration file with the command
-function update_config_file {
-    local config_file="$1"
-
-    if [[ -w "$config_file" ]]; then
-        if ! grep -q "$COMMAND_TO_EXPORT" "$config_file"; then
-            read -p "Do you want to update PATH in $config_file? [y/n] " -n 1 -r
-            echo
-
-            if [[ $REPLY =~ ^[Yy]$ ]]; then
-                echo "Updating $config_file ..."
-                echo "export PATH=\"\$PATH:$COMMAND_TO_EXPORT\"" >> "$config_file"
-                echo "Command added to PATH in $config_file."
-            else
-                echo "Skipping update of $config_file."
-            fi
+# function to check if a file exists and is writable
+function check_file_writable {
+    local file="$1"
+    if [[ -e "$file" ]]; then
+        if [[ -w "$file" ]]; then
+            return 0
         else
-            echo "Command already in PATH in $config_file; skipping update."
+            echo "Error: $file is not writable"
+            return 1
         fi
     else
-        echo "Error: Cannot write to $config_file. Please check file permissions."
+        echo "Error: $file does not exist"
+        return 1
     fi
+}
+
+# function to prompt the user for input
+function prompt_user {
+    local message="$1"
+    local default_value="$2"
+    local allowed_values="$3"
+    
+    read -p "$message" -i "$default_value" -e value
+    if [[ -n "$allowed_values" ]]; then
+        if [[ "$allowed_values" == *"$value"* ]]; then
+            echo "$value"
+        else
+            echo "$default_value"
+        fi
+    else
+        echo "$value"
+    fi
+}
+
+# function to add the command to the PATH in the config file
+function add_command_to_path {
+    local config_file="$1"
+    
+    if grep -q "$COMMAND_TO_EXPORT" "$config_file"; then
+        echo "PATH already updated in $config_file; skipping update."
+        return
+    fi
+    
+    local add_to_path=$(prompt_user "Do you want to add $COMMAND_TO_EXPORT to PATH in $config_file? [y/n]: " "y" "yn")
+    
+    if [[ "$add_to_path" == "y" ]]; then
+        echo "Updating $config_file ..."
+        echo "export PATH=\"\$PATH:$COMMAND_TO_EXPORT\"" >> "$config_file"
+    else
+        echo "Skipping update of $config_file."
+    fi
+}
+
+# function to update the appropriate config file based on the shell
+function update_config_file {
+    local shell="$1"
+    
+    case "$shell" in
+        *zsh)
+            local zshrc="$HOME/.zshrc"
+            local zshenv="$HOME/.zshenv"
+            if check_file_writable "$zshrc"; then
+                add_command_to_path "$zshrc"
+            elif check_file_writable "$zshenv"; then
+                add_command_to_path "$zshenv"
+            else
+                echo "Error: Cannot find a writable zsh config file"
+            fi
+            ;;
+        *bash)
+            local bashrc="$HOME/.bashrc"
+            local bash_profile="$HOME/.bash_profile"
+            if check_file_writable "$bash_profile"; then
+                add_command_to_path "$bash_profile"
+            elif check_file_writable "$bashrc"; then
+                add_command_to_path "$bashrc"
+            else
+                echo "Error: Cannot find a writable bash config file"
+            fi
+            ;;
+        *)
+            echo "Error: Unknown shell type $shell"
+            ;;
+    esac
 }
 
 SNOWBALL_INSTALL_DIR=~/.snowball
@@ -48,26 +110,12 @@ EXPORT_COMMAND="export PATH=$(pwd)/bin:\$PATH"
 echo "PATH export command:"
 echo "  $EXPORT_COMMAND"
 
-# check the shell and update the appropriate config file
-if [[ "$SHELL" == *zsh ]]; then
-    if [[ -e "$HOME/.zshrc" ]]; then
-        update_config_file "$HOME/.zshrc"
-    elif [[ -e "$HOME/.zshenv" ]]; then
-        update_config_file "$HOME/.zshenv"
-    else
-        echo "Could not find a zsh configuration file to update PATH"
-    fi
-elif [[ "$SHELL" == *bash ]]; then
-    if [[ -e "$HOME/.bash_profile" ]]; then
-        update_config_file "$HOME/.bash_profile"
-    elif [[ -e "$HOME/.bashrc" ]]; then
-        update_config_file "$HOME/.bashrc"
-    else
-        echo "Could not find a bash configuration file to update PATH"
-    fi
-else
-    echo "Error: Unknown shell type $SHELL"
+local shell="$SHELL"
+if [[ -z "$shell" ]]; then
+    shell=$(ps -p $$ -o args= | awk '{print $1}')
 fi
+
+update_config_file "$shell"
 
 echo "Snowball successfully installed at: $(pwd)"
 echo "Open a new terminal session or update your PATH to use snowball"
