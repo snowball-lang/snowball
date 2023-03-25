609e850be3b7d32ceee324b9f8fb441308ec1075
mauro-balades <mauro.balades@tutanota.com>
2023-03-25 23:15:27 +0100

    Changed snowball library and included it into the release files

--- scripts/install.sh
+++ scripts/install.sh
@@ -20,6 +20,7 @@ cd $SNOWBALL_INSTALL_DIR
 curl -L https://github.com/snowball-lang/snowball/releases/latest/download/"$SNOWBALL_BUILD_ARCHIVE" | tar zxvf - --strip-components=1
 
 sudo mv libSnowballRuntime.so $LIB_FOLDER
+sudo mv libSnowball.so $LIB_FOLDER
 
 EXPORT_COMMAND="$(pwd)/bin"
 echo "PATH export command:"
