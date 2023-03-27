19f4266a1d9aa034af3083253f4812a9f4079cbf
mauro-balades <mauro.balades@tutanota.com>
2023-03-27 16:58:33 +0200

    Added a flag to remove debuging on release

--- build_scripts/release.sh
+++ build_scripts/release.sh
@@ -1,3 +1,5 @@
+export CXXFLAGS="-D_SN_DEBUG=0"
+
 mkdir -p bin/Release
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release -D_SN_DEBUG=OFF -DEXECUTABLE_OUTPUT_PATH="bin/Release" $@ .
 cmake --build . --config Release
