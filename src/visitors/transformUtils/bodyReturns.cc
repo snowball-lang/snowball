7bd4092f088e83d31e44e3cd73f17c20a5c8f685
mauro-balades <mauro.balades@tutanota.com>
2023-05-06 12:49:40 +0200

    Checking if conditional statements return on all paths

--- src/visitors/transformUtils/bodyReturns.cc
+++ src/visitors/transformUtils/bodyReturns.cc
@@ -12,13 +12,14 @@ bool Transformer::bodyReturns(std::vector<Node *> exprs) {
             return true;
         } else if (auto b = cast<Block>(expr)) {
             if (bodyReturns(b->getStmts())) return true;
-        } else if (auto b = cast<Statement::Conditional>(expr)) {
-            assert(false);
+        } else if (auto c = cast<Statement::Conditional>(expr)) {
+            auto elseBlock = c->getElse();
+            if (bodyReturns(c->getBlock()->getStmts()) && elseBlock && bodyReturns(elseBlock->getStmts())) {
+                return true;
+            }
         }
         // TODO: Break, continue: return false
         // TODO: Loop, while: if (bodyReturns(b->getStmts())) return true;
-        // TODO: If: if bodyReturns(if_body) && bodyReturns(else_body) { return
-        // true }
 
         // Ignore unhandled!
     }
