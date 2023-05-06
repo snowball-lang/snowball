a28b6bb770975c0ef3214f47d9418501fae848ea
mauro-balades <mauro.balades@tutanota.com>
2023-05-06 12:58:19 +0200

    (WIP): Transforming conditional statements into IR

--- src/visitors/transformVisits/Statement/Conditional.cc
+++ src/visitors/transformVisits/Statement/Conditional.cc
@@ -6,7 +6,24 @@ using namespace snowball::Syntax::transform;
 namespace snowball {
 namespace Syntax {
 
-SN_TRANSFORMER_VISIT(Statement::Conditional) { assert(false); }
+SN_TRANSFORMER_VISIT(Statement::Conditional) {
+    p_node->getCondition()->accept(this);
+    auto expr = getBooleanValue(this->value);
+
+    p_node->getBlock()->accept(this);
+    auto truthyBody =
+        utils::dyn_cast<ir::Block>(this->value);
+
+    std::shared_ptr<Block> falsyBody = nullptr;
+    if (auto x = p_node->getElse()) {
+        x->accept(this);
+        falsyBody = utils::dyn_cast<ir::Block>(this->value);
+    }
+
+    auto cond = ctx->module->N<ir::Conditional>(p_node->getDBGInfo(), expr,
+        truthyBody, falsyBody);
+    this->value = cond;
+}
 
 } // namespace Syntax
 } // namespace snowball
