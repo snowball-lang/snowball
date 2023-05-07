69fafec98d197f228a9243bc60868a3abfd77e49
mauro-balades <mauro.balades@tutanota.com>
2023-05-07 14:16:10 +0200

    Export gcc for non-macos users

--- build_scripts/release.sh
+++ build_scripts/release.sh
@@ -1,5 +1,12 @@
 export CXXFLAGS="-D_SN_DEBUG=0"
 
+if [[ "$OSTYPE" != "darwin"* ]]; then
+    export C="/usr/bin/g++"
+    export CXX="/usr/bin/g++-9"
+
+    g++ --help
+fi
+
 mkdir -p bin/Release
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DLLVM_ENABLE_BACKTRACES=OFF \
 -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=OFF \
