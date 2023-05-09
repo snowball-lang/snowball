1847132cb48ccfac9bf54d53f3c723c10172d39f
mauro-balades <mauro.balades@tutanota.com>
2023-05-09 18:48:35 +0200

    Fixed LD args for both macOS and linux

--- src/ld_args.h
+++ src/ld_args.h
@@ -10,37 +10,27 @@
             "-dynamic-linker"                                                  \
             "-L" STATICLIB_DIR,                                                \
             LLVM_LDFLAGS, "-L" STATICLIB_DIR "" _SNOWBALL_LIBRARY_DIR,         \
-            p_input, "-lSnowballRuntime", "-lc", "-lgcc", "-lm"                \
+            p_input, "-lSnowballRuntime", "-lc", "-lm"                \
     }
 #elif __APPLE__
 #define LD_ARGS()                                                              \
     {                                                                          \
-        LD_PATH, "-execute", "/lib64/ld-linux-x86-64.so.2",             \
-            "/usr/lib/gcc/x86_64-linux-gnu/" LIBC_VERSION "/crtbegin.o",       \
-            "/usr/lib/gcc/x86_64-linux-gnu/" LIBC_VERSION "/crtend.o",         \
-            "/usr/lib/x86_64-linux-gnu/crt1.o",                                \
-            "/usr/lib/x86_64-linux-gnu/crti.o",                                \
-            "/usr/lib/x86_64-linux-gnu/crtn.o",                                \
+        LD_PATH, "-execute", \
             "-L/usr/lib/gcc/x86_64-linux-gnu/" LIBC_VERSION "/",               \
             "-L/usr/lib/gcc/x86_64-linux-gnu/" LIBC_VERSION "/",               \
             "-L" STATICLIB_DIR, LLVM_LDFLAGS,                                  \
             "-L" STATICLIB_DIR PATH_SEPARATOR _SNOWBALL_LIBRARY_DIR, p_input,              \
-            "-lSnowballRuntime", "-lc", "-lgcc", "-lm"                         \
+            "-lSnowballRuntime", "-lc",  "-lm"                         \
     }
 #elif __linux__
 #define LD_ARGS()                                                              \
     {                                                                          \
-        LD_PATH, "-dynamic-linker", "/lib64/ld-linux-x86-64.so.2",             \
-            "/usr/lib/gcc/x86_64-linux-gnu/" LIBC_VERSION "/crtbegin.o",       \
-            "/usr/lib/gcc/x86_64-linux-gnu/" LIBC_VERSION "/crtend.o",         \
-            "/usr/lib/x86_64-linux-gnu/crt1.o",                                \
-            "/usr/lib/x86_64-linux-gnu/crti.o",                                \
-            "/usr/lib/x86_64-linux-gnu/crtn.o",                                \
+        LD_PATH, "-dynamic-linker", \
             "-L/usr/lib/gcc/x86_64-linux-gnu/" LIBC_VERSION "/",               \
             "-L/usr/lib/gcc/x86_64-linux-gnu/" LIBC_VERSION "/",               \
             "-L" STATICLIB_DIR, LLVM_LDFLAGS,                                  \
             "-L" STATICLIB_DIR PATH_SEPARATOR _SNOWBALL_LIBRARY_DIR, p_input,              \
-            "-lSnowballRuntime", "-lc", "-lgcc", "-lm"                         \
+            "-lSnowballRuntime", "-lc", "-lm"                         \
     }
 #else
 #error "Unknown compiler"
