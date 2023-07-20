218a4b0cb5026c708938ef299211cc74f18d3c05
mauro <mauro.balades@tutanota.com>
2023-07-20 15:58:05 +0200

    Fixed scape characters

--- build_scripts/release_build.sh
+++ build_scripts/release_build.sh
@@ -20,7 +20,7 @@ error() {
 if [[ "$OSTYPE" == "darwin"* ]]; then
     if [[ "$ARCH" == "arm64" ]]; then
         mkdir arm-homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C arm-homebrew 
-        alias arm-brew='$(pwd)/arm-homebrew/bin/brew'
+        alias arm-brew="$(pwd)/arm-homebrew/bin/brew"
         response=$(arm-brew fetch --force --bottle-tag=arm64_big_sur boost | grep "Downloaded to")
         parsed=($response)  
         arm-brew install $parsed[3] 
