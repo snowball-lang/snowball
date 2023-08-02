d6c1b1526532f0fa9affbde1a7860078a78ff75b
mauro-balades <mauro.balades@tutanota.com>
2023-08-02 10:40:45 +0200

    Fixed symbol exports for macOS

--- runtime/libs/sym.h
+++ runtime/libs/sym.h
@@ -3,7 +3,7 @@
 #define _SNOWBALL_SYMBOLS_EXPORT_H_
 
 #if defined(__MACOSX__) || defined(__APPLE__) || defined(__MACH__)
-#define _SN_SYM(x) __asm__("\x01" x)
+#define _SN_SYM(x) __asm__("_" x)
 #else 
 #define _SN_SYM(x) __asm__(x)
 #endif
