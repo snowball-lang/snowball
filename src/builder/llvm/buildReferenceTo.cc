b51d64ac545a117be686a3fd6104720173ebdf77
mauro-balades <mauro.balades@tutanota.com>
2023-12-25 22:15:17 +0100

    Version 9!

--- src/builder/llvm/buildReferenceTo.cc
+++ src/builder/llvm/buildReferenceTo.cc
@@ -13,17 +13,6 @@ void LLVMBuilder::visit(ir::ReferenceTo* ref) {
   auto val = ref->getValue();
   auto llvmReferencedValue = build(val.get());
 
-  // if (!llvmReferencedValue->getType()->isPointerTy() || ctx->doNotLoadInMemory) {
-  //   auto tempVal = createAlloca(getLLVMType(val->getType()));
-  //   builder->CreateStore(llvmReferencedValue, tempVal);
-  //   llvmReferencedValue = tempVal;
-  // }
-
-  // if (llvm::isa<llvm::LoadInst>(llvmReferencedValue)) {
-  //   llvmReferencedValue = llvm::cast<llvm::LoadInst>(llvmReferencedValue)->getPointerOperand();
-  // }
-
-  // builder->CreateStore(llvmReferencedValue, tempVal);
   auto tempVal = (llvm::Value*) nullptr;
   if (llvm::isa<llvm::Constant>(llvmReferencedValue)) {
     auto var = new llvm::GlobalVariable(
