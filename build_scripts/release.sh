f11b6440a856d56d1a9727ca24a658378cf4941b
mauro-balades <mauro.balades@tutanota.com>
2024-01-04 18:20:41 +0100

    Trying to fix release builds

--- build_scripts/release.sh
+++ build_scripts/release.sh
@@ -1,20 +1,11 @@
-if [[ "$OSTYPE" != "darwin"* ]]; then
-    sudo apt install software-properties-common
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-    sudo apt update
-    sudo apt install gcc-9 g++-9 -y
-    sudo apt install cmake -y
-
-    export CC="/usr/bin/gcc-9"
-    export CXX="/usr/bin/g++-9"
-fi
-
 mkdir -p bin/Release
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local -DLLVM_ENABLE_BACKTRACES=OFF \
+cmake  -DCMAKE_INSTALL_PREFIX:PATH=/usr/local -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+-DLLVM_ENABLE_BACKTRACES=OFF \
 -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=OFF \
 -DLLVM_ENABLE_TERMINFO=OFF \
 -DLLVM_ENABLE_ZLIB=OFF \
 -DLLVM_INCLUDE_EXAMPLES=OFF \
--DCMAKE_OSX_ARCHITECTURES="$ARCH" \
--DLLVM_INCLUDE_DOCS=OFF -DCMAKE_BUILD_TYPE=Release -DEXECUTABLE_OUTPUT_PATH="bin/Release" $@ .
-cmake --build . --config Release -- -j 3
+-DCMAKE_OSX_ARCHITECTURES="$ARCHS" \
+-DLLVM_INCLUDE_DOCS=OFF -DEXECUTABLE_OUTPUT_PATH="bin/Release" .
+cmake --build . --config Release -- $@
