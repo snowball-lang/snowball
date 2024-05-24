967b4bff8766af5a0ffac739683dabcb79cad5c2
Mauro Balades <mauro.balades@tutanota.com>
2024-05-24 17:08:08 +0200

    chore: Update sn.confy template to include default project description

--- src/app/src/init.cc
+++ src/app/src/init.cc
@@ -63,6 +63,9 @@ void CLI::make_init(Ctx& ctx, Args& args, bool new_project) {
   if (version.empty()) {
     version = "0.1.0";
   }
+  if (description.empty()) {
+    description = "A cool Snowball project!";
+  }
 
   if (new_project) {
     auto path = std::filesystem::current_path() / name.getValue();
@@ -97,7 +100,7 @@ void CLI::make_init(Ctx& ctx, Args& args, bool new_project) {
   Logger::status("Creating", "Project files");
   std::filesystem::create_directory("src");
   write_file("src/main.sn", lib_type == LibType::Exe ? EXECUTABLE_MAIN : LIBRARY_MAIN);
-  write_file("sn.confy", fmt::format(SN_CONFY, name, version, description, lib_type_str));
+  write_file("sn.confy", fmt::format(SN_CONFY, name, description, version, lib_type_str));
   write_file(".gitignore", _GITIGNORE);
   write_file("sn.reky", SN_REKY);
 
