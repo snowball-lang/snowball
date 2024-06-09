9c9cbd3bc8160c6885925757f8979d6dd9148946
Mauro Balades <mauro.balades@tutanota.com>
2024-06-09 22:59:39 +0200

    refactor: Update UUID library structure in type checker

--- src/compiler/frontend/sema/uuids.h
+++ src/compiler/frontend/sema/uuids.h
@@ -5,10 +5,10 @@
 static const std::vector<std::string> core_string = {"std", "string"};
 
 // This is a list of allowed UUIDs that can be used in the type checker
-// note: They should ALWAYS follow "core"'s library structure
+// note: They should ALWAYS follow std's library structure
 #define ALLOWED_UUIDS std::vector{\
-  NamespacePath(""),\
   NamespacePath(core_string),\
+  NamespacePath(""),\
   }
 
 #endif // __SNOWBALL_FRONTEND_SEMA_UUIDS_H__
