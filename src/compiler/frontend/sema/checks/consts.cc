25f57ff54a93110eed3fd9f5fe70736359a0b1e5
Mauro Balades <mauro.balades@tutanota.com>
2024-06-11 20:20:54 +0200

    chore: Update string prefix in TypeChecker and Binder classes

--- src/compiler/frontend/sema/checks/consts.cc
+++ src/compiler/frontend/sema/checks/consts.cc
@@ -34,15 +34,15 @@ void TypeChecker::visit(ast::Number* node) {
 
 void TypeChecker::visit(ast::String* node) {
   switch (node->get_prefix()) {
-    case 'b':
-    case 'B':
+    case 'c':
+    case 'C':
       unify(node->get_type(), get_type("u8")->get_pointer_to(false), node->get_location());
       break;
     case 0:
       sn_assert(false, "Normal strings are not supported yet!");
     default: err(node->get_location(), "Invalid string prefix!", Error::Info {
       .highlight = "Invalid string prefix",
-      .help = "Valid string prefixes are 'b' and 'B' for byte strings."
+      .help = "Valid string prefixes are 'c' and 'C' for byte strings."
     }, Error::Type::Err, false);
   }
 }
--- src/compiler/sil/consts.cc
+++ src/compiler/sil/consts.cc
@@ -16,7 +16,7 @@ void Binder::visit(ast::String* node) {
   auto val = node->get_value();
   // Remove the parsed quotes form the value
   val = val.substr(1, val.size() - 2);
-  if (node->get_prefix() == 'b' || node->get_prefix() == 'B') {
+  if (node->get_prefix() == 'c' || node->get_prefix() == 'C') {
     value = ConstString::create(node->get_location(), get_type(node), val);
     return;
   }
