1d19d3018cce0f3941bad0e6163c2091005c23a8
mauro 🤙 <mauro.balades@tutanota.com>
2024-06-20 17:39:30 +0000

    chore: Update .clang-tidy to include additional file extensions for header and implementation files

--- scripts/clang-tidy.sh
+++ scripts/clang-tidy.sh
@@ -1,19 +1,10 @@
 
 cc_files=$(find src/ -name "*.cc")
-header_files=$(find src/ -name "*.h")
 
 run_clang_tidy() {
   clang-tidy $1 --fix --fix-errors -p $(pwd)/compile_commands.json --quiet
 }
 
-run_clang_on_files() {
-  for file in $1; do
-    run_clang_tidy $file
-  done
-}
-
 # First the source files
-run_clang_on_files "$cc_files"
+run_clang_tidy "$cc_files"
 
-# Then the header files
-run_clang_on_files "$header_files"
