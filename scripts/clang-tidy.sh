1a734104039d3f9ab1fca455339f55fa079c112f
mauro 🤙 <mauro.balades@tutanota.com>
2024-06-20 17:43:30 +0000

    chore: Update .clang-tidy to remove quiet flag and fix compile_commands.json path

--- scripts/clang-tidy.sh
+++ scripts/clang-tidy.sh
@@ -1,8 +1,8 @@
-
+set -ex
 cc_files=$(find src/ -name "*.cc")
 
 run_clang_tidy() {
-  clang-tidy $1 --fix --fix-errors -p $(pwd)/compile_commands.json --quiet
+  clang-tidy $1 --fix --fix-errors -p $(pwd)/compile_commands.json
 }
 
 # First the source files
