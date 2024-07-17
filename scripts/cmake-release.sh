3e152c257ffd2bdce52219aeb7078cebcf56f3e5
mauro 🤙 <mauro.balades@tutanota.com>
2024-07-17 21:40:33 +0000

    deleted the project

--- scripts/cmake-release.sh
+++
@@ -1,8 +0,0 @@
-
-mkdir build
-cd build
-
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Werror" \
-      ..
-
---
+++ xmake.lua
@@ -0,0 +1,28 @@
+set_project("Snowball")
+set_version("1.0.0")
+
+set_languages("c++latest")
+
+add_rules("mode.debug", "mode.release")
+
+-- Set all possible warnings
+set_warnings("all", "error")
+
+-- Setup for debug symbols and optimization
+if is_mode("debug") then
+    set_symbols("debug")
+    set_optimize("none")
+else
+    set_symbols("hidden")
+    set_optimize("fastest")
+    set_strip("all")
+end
+
+-- Function to add platform-specific flags
+function add_platform_specific_flags()
+    if is_plat("windows") then
+        add_cxxflags("/EHsc")
+    else
+        -- Add flags for e.g. linux here
+    end
+end
