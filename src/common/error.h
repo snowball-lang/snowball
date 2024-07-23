e773ab51262c4539a00efa85ae24ba2b237eec0e
Mauro Balades <mauro.balades@tutanota.com>
2024-07-23 21:01:15 +0200

    refactor: Remove unused files, export main module, and improve code readability

--- src/common/error.h
+++ src/common/error.h
@@ -39,15 +39,32 @@ public:
     String mMessage{};
     Kind mKind{Error};
   };
-
   /// @brief Construct a new source error.
   /// @param data The source error data.
   explicit SourceError(const Vector<SourceErrorData>& data) : mData(data) {}
   /// @brief Get the source error data.
   /// @return The source error data.
-  auto GetData() const -> const Vector<SourceErrorData>& { return mData; }
+  auto GetData() const -> const Vector<SourceErrorData>&;
+
+  /// @brief Append a new Info source error.
+  auto WithInfo(const String& message, const std::optional<SourceLocation>&
+    location = std::nullopt) -> SourceError&;
+  /// @brief Append a new Note source error.
+  auto WithNote(const String& message, const std::optional<SourceLocation>&
+    location = std::nullopt) -> SourceError&;
+  /// @brief Append a new Warning source error.
+  auto WithWarning(const String& message, const std::optional<SourceLocation>&
+    location = std::nullopt) -> SourceError&;
 private:
   Vector<SourceErrorData> mData;
 };
+
+/// @brief It displays an error message and immediately exits the program.
+/// @param error The error to display.
+[[noreturn]] auto FatalError(const Error& error) -> void;
+
+// Group: fmt utils
+auto format_as(const Error& error) -> String; 
+auto format_as(const SourceError& error) -> String;
 
 } // namespace snowball
